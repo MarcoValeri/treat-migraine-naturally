c936e33e107d71a125b5947c88b169a2e4302533
MarcoValeri <marcovaleri@hotmail.it>
2021-03-01 11:29:16 +0000

    Connect application to Google Cloud MySQL database

--- private/db/db_credentials.php
+++ private/db/db_credentials.php
@@ -1,8 +1,13 @@
 <?php
     // Create a variables where saving db credentials
-    define("DB_SERVER", "localhost");
-    define("DB_USER", 'marco_tmn');
-    define("DB_PASS", 'B!rkbeck01');
-    define("DB_NAME", "treat_migraine_naturally");
+    // define("DB_SERVER", "localhost");
+    // define("DB_USER", 'marco_tmn');
+    // define("DB_PASS", 'B!rkbeck01');
+    // define("DB_NAME", "treat_migraine_naturally");
+
+    define("DB_SERVER", "35.242.164.97");
+    define("DB_USER", "marco_tmn");
+    define("DB_PASS", "S!lver09");
+    define("DB_NAME", "treatMigraineNaturally");
 
 ?>
