00d13d927ef4d07ca195e37563e41f3814ab5e50
MarcoValeri <marcovaleri@hotmail.it>
2021-04-07 19:25:28 +0100

    Added style to user-admin-menu.php for large displays

--- public/admin/users/users.php
+++ public/admin/users/users.php
@@ -27,46 +27,53 @@ include(INCLUDE_PATH . '/header.php');
 
 <!-- Main -->
 <main class="users-main">
-    <section class="users-main-newuser">
-        <h2>Add a new user</h2>
-        <a href="<?= url_for('/admin/users/add.php'); ?>">New User</a>
-</section>
-
-    <section  class="users-main-list">
-        <h2>Treat Migraine Naturally users</h2>
-        <table>
-            <tr>
-                <th>id</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Admin</th>
-                <th>View</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-            <?php 
-                /*
-                * Call the function find_users() that gets all data by the
-                * users db and print them out into the table
-                */
-                $users = find_users($db);
-                while($user = mysqli_fetch_assoc($users)) {
-            ?>
-            <tr>    
-                <td><?= $user['id'] ?></td>
-                <td><?= $user['first_name'] ?></td>
-                <td><?= $user['last_name'] ?></td>
-                <td><?= $user['email'] ?></td>
-                <td><?= $user['password'] ?></td>
-                <td><?= $user['admin'] ?></td>
-                <td><a href="<?= url_for('/admin/users/show.php?id=' . $user['id']); ?>">View</a></td>
-                <td><a href="<?= url_for('/admin/users/edit.php?id=' . $user['id']); ?>">Edit</a></td>
-                <td><a href="<?= url_for('/admin/users/delete.php?id=' . $user['id']); ?>">Delete</a></td>
-            </tr>
-            <?php } ?>
-        </table>
+    <section class="user-main-gridcontainer">
+        <section class="user-main-gridcontainer-newuser">
+            <h2>Add a new user</h2>
+            <div class="user-main-gridcontainer-newuser-btn-wrap">
+                <a href="<?= url_for('/admin/users/add.php'); ?>">
+                    <div class="user-main-gridcontainer-newuser-btn">
+                        New User
+                    </div>
+                </a>
+            </div>
+        </section>
+        <section  class="user-main-gridcontainer-list">
+            <h2>Treat Migraine Naturally users</h2>
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Admin</th>
+                    <th>View</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                <?php 
+                    /*
+                    * Call the function find_users() that gets all data by the
+                    * users db and print them out into the table
+                    */
+                    $users = find_users($db);
+                    while($user = mysqli_fetch_assoc($users)) {
+                ?>
+                <tr>    
+                    <td><?= $user['id'] ?></td>
+                    <td><?= $user['first_name'] ?></td>
+                    <td><?= $user['last_name'] ?></td>
+                    <td><?= $user['email'] ?></td>
+                    <td><?= $user['password'] ?></td>
+                    <td><?= $user['admin'] ?></td>
+                    <td><a href="<?= url_for('/admin/users/show.php?id=' . $user['id']); ?>">View</a></td>
+                    <td><a href="<?= url_for('/admin/users/edit.php?id=' . $user['id']); ?>">Edit</a></td>
+                    <td><a href="<?= url_for('/admin/users/delete.php?id=' . $user['id']); ?>">Delete</a></td>
+                </tr>
+                <?php } ?>
+            </table>
+        </section>
     </section>
 </main>
 
