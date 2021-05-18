38d9a0cd101cba333d3ffe98094a5f11fcc5f091
Marco Valeri <marcovaleri@hotmail.it>
2021-05-18 18:47:06 +0200

    Add file .htaccess with some new rules for urls

--- private/db/db_credentials.php
+++ private/db/db_credentials.php
@@ -10,9 +10,14 @@
     // define("DB_PASS", "S!lver09");
     // define("DB_NAME", "treatMigraineNaturally");
 
-    define("DB_SERVER", "chegiornoeoggi.com");
-    define("DB_USER", "ug2iusgcymiwi");
-    define("DB_PASS", "tu2n191(4@&2");
-    define("DB_NAME", "dbmiyqteieeurl");
+    // define("DB_SERVER", "chegiornoeoggi.com");
+    // define("DB_USER", "ug2iusgcymiwi");
+    // define("DB_PASS", "tu2n191(4@&2");
+    // define("DB_NAME", "dbmiyqteieeurl");
+
+    define("DB_SERVER", "treatmigrainenaturally.co.uk");
+    define("DB_USER", "uy8bbplwdvyx6");
+    define("DB_PASS", ",k43k7@T?n1o");
+    define("DB_NAME", "dbwkfghufkg1mb");
 
 ?>
